8ecf30d6a1cff8a05fc30e3760e8aa5f1a7f0695
subham mishra <subham.mishra14@gmail.com>
2020-05-14 15:49:06 +0530

    register the apps for the project

--- api/picture_search_api/picture_search_api/settings.py
+++ api/picture_search_api/picture_search_api/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.coordinates',
+    'apps.favourite_photos'
 ]
 
 MIDDLEWARE = [
