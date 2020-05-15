118905ef9cb09aec92e7dc47f095c22f43a8eb0f
subham mishra <subham.mishra14@gmail.com>
2020-05-15 15:47:36 +0530

    Added bootstrap 4 and created Navbar

---
+++ frontend/frontend/picture_search_user_interface/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from '../src/components/navbar'
+
+
+const  App = props => {
+  return (
+
+    <div>
+
+      <Navbar/>
+
+    </div>
+  );
+}
+
+export default App;
