0446e30c964af4d1c60178f14f62401b763bed4b
subham mishra <subham.mishra14@gmail.com>
2020-05-19 17:37:54 +0530

    change for fetching preset location data

--- frontend/picture_search_user_interface/src/containers/homePageSearch.js
+++ frontend/picture_search_user_interface/src/containers/homePageSearch.js
@@ -1,24 +1,59 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import LatLongForm from '../components/forms/latLongForm'
 import SelectForm from '../components/forms/selectForm'
-
+import Button from 'react-bootstrap/Button'
+import * as actions from '../store/actions/index'
+import {connect} from 'react-redux'
 
 const HomePageSearch = props => {
 
-    const [form, setform] = useState("lat_long_form")
+    useEffect(() => {
+        props.onPlaceList()
+    }, [])
+
+    let [form, setform] = useState("latLongForm")
+
+    const  showLatLongForm = () => {
+        setform ("latLongForm")
+    }
+
+    const  showNameForm = () => { 
+        setform ("nameForm")
+    }
     
     return (
+        
         <div>
         
         <br></br>
         <br></br>
         <br></br>
         <br></br>
-        <LatLongForm></LatLongForm>
+        <Button variant="outline-info" size="sm" onClick={showLatLongForm} >By Lat-Long</Button>{" "}
+        <Button variant="outline-info" size="sm" onClick={showNameForm} >By Name</Button>{" "}
         <br></br>
-        <SelectForm></SelectForm>
+        <br></br>
+        {
+            form === "latLongForm" ? <LatLongForm></LatLongForm> : <SelectForm options={props.options}></SelectForm>
+        }
+        
+        <br></br>
+        
         </div>
     );
 };
 
-export default HomePageSearch
+const mapStateToProps = state => {
+    return{
+        options: state.placeList.placeList
+    }
+
+}
+
+const mapDispatchToProps = dispatch => {
+    return{
+        onPlaceList: () => dispatch(actions.placeFetch())
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps) (HomePageSearch)
