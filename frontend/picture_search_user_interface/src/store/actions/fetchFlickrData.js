6bed0ecc044302d82a42a10f65e25a1a1148b239
subham mishra <subham.mishra14@gmail.com>
2020-05-21 12:25:17 +0530

    Removing lat, long hard code value and using values passed to the function

--- frontend/picture_search_user_interface/src/store/actions/fetchFlickrData.js
+++ frontend/picture_search_user_interface/src/store/actions/fetchFlickrData.js
@@ -22,7 +22,7 @@ export const fetchFlickrDataFail = (error) => {
     };
 };
 
-export const fetchFlickrData = () => {
+export const fetchFlickrData = (lat, long) => {
     
     return dispatch => {
         dispatch(fetchFlickrDataStart())
@@ -30,15 +30,12 @@ export const fetchFlickrData = () => {
         let baseUrl = 'https://www.flickr.com/services/rest/'
         let method = 'flickr.photos.search'
         let api_key = process.env.REACT_APP_FLICKR_API_KEY
-        console.log(process.env);
         
         let accuracy = '11'
-        let lat =  '-27.46'
-        let lon = '153.02'
         let perPage = '3'
         let page = '1'
         let format = 'json'
-        let url = baseUrl+'?method='+method+'&api_key='+api_key+'&accuracy='+accuracy+'&lat='+lat+'&lon='+lon+'&per_page='+perPage+'&page='+page+'&format='+format
+        let url = baseUrl+'?method='+method+'&api_key='+api_key+'&accuracy='+accuracy+'&lat='+lat+'&lon='+long+'&per_page='+perPage+'&page='+page+'&format='+format
         
         axios.get(url)
         .then(result => {
