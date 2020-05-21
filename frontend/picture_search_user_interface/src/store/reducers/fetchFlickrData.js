675a1341d77a91f44511b6e1bd611a4e71de88a0
subham mishra <subham.mishra14@gmail.com>
2020-05-21 17:03:45 +0530

    Renaming the file

--- frontend/picture_search_user_interface/src/store/reducers/fetchFlickrData.js
+++ frontend/picture_search_user_interface/src/store/reducers/fetchFlickrData.js
@@ -2,8 +2,12 @@ import * as actionTypes from '../actions/actionTypes'
 import { updateObject } from '../../shared/utility'
 
 const initialState = {
-    pictureList: [],
-    error: null
+    photoList: null,
+    error: null,
+    page: 1,
+    lat: null,
+    long: null
+
 };
 
 const fetchFlickrDataStart = (state, action) => {
@@ -13,7 +17,12 @@ const fetchFlickrDataStart = (state, action) => {
 const fetchFlickrDataSuccess = (state, action) => {
     return updateObject(state, {
         error: null,
-        photoList: action.photoList
+        photoList: action.photoList,
+        page: action.page,
+        lat: action.lat,
+        long: action.long,
+
+
     })
 }
 
@@ -22,12 +31,35 @@ const fetchFlickrDataFail = (state, action) => {
         error: action.error
     })
 }
+//-----fetch lat long 
+
+const latLongFetchStart = (state, action) => {
+    return updateObject(state, {error: null})
+};
+
+const latLongFetchSuccess = (state, action) => {
+    return updateObject(state, {
+        error: null,
+        lat: action.lat,
+        long: action.long
+    })
+}
+
+const latLongFetchFail = (state, action) => {
+    return updateObject(state, {
+        error: action.error
+    })
+}
+
 
 const reducer = (state=initialState, action) => {
     switch (action.type){
         case actionTypes.FETCH_FLICKR_DATA_START: return fetchFlickrDataStart(state, action)
         case actionTypes.FETCH_FLICKR_DATA_SUCCESS: return fetchFlickrDataSuccess(state, action)
         case actionTypes.FETCH_FLICKR_DATA_FAIL: return fetchFlickrDataFail(state, action)
+        case actionTypes.FETCH_LAT_LONG_DATA_START: return latLongFetchStart(state, action)
+        case actionTypes.FETCH_LAT_LONG_DATA_SUCCESS: return latLongFetchSuccess(state, action)
+        case actionTypes.FETCH_LAT_LONG_DATA_FAIL: return latLongFetchFail(state, action)
         default:
             return state
     }
