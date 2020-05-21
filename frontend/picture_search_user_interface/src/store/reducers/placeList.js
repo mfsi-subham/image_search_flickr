95ea2f89a03df5678f3886fcdedc7e391e0338c8
subham mishra <subham.mishra14@gmail.com>
2020-05-21 12:23:32 +0530

    creating fetchLatLongData reducers

--- frontend/picture_search_user_interface/src/store/reducers/placeList.js
+++ frontend/picture_search_user_interface/src/store/reducers/placeList.js
@@ -3,7 +3,9 @@ import { updateObject } from '../../shared/utility'
 
 const initialState = {
     placeList: [],
-    error: null
+    error: null,
+    lat: null,
+    long: null
 };
 
 const placeFetchStart = (state, action) => {
@@ -23,11 +25,34 @@ const placeFetchFail = (state, action) => {
     })
 }
 
+//-----fetch lat long 
+const latLongFetchStart = (state, action) => {
+    return updateObject(state, {error: null})
+};
+
+const latLongFetchSuccess = (state, action) => {
+    return updateObject(state, {
+        error: null,
+        lat: action.lat,
+        long: action.long
+    })
+}
+
+const latLongFetchFail = (state, action) => {
+    return updateObject(state, {
+        error: action.error
+    })
+}
+
+
 const reducer = (state=initialState, action) => {
     switch (action.type){
         case actionTypes.PLACE_FETCH_START: return placeFetchStart(state, action)
         case actionTypes.PLACE_FETCH_SUCCESS: return placeFetchSuccess(state, action)
         case actionTypes.PLACE_FETCH_FAIL: return placeFetchFail(state, action)
+        case actionTypes.FETCH_LAT_LONG_DATA_START: return latLongFetchStart(state, action)
+        case actionTypes.FETCH_LAT_LONG_DATA_SUCCESS: return latLongFetchSuccess(state, action)
+        case actionTypes.FETCH_LAT_LONG_DATA_FAIL: return latLongFetchFail(state, action)
         default:
             return state
     }
