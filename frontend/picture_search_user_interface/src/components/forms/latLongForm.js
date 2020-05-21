784e31fe0d4a05ccc03e9f163d25f9f524f7a980
subham mishra <subham.mishra14@gmail.com>
2020-05-21 12:26:03 +0530

    Using useState hook to manage form data

--- frontend/picture_search_user_interface/src/components/forms/latLongForm.js
+++ frontend/picture_search_user_interface/src/components/forms/latLongForm.js
@@ -1,23 +1,49 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {Form, Col} from 'react-bootstrap'
 import Button from 'react-bootstrap/Button'
+import {connect} from 'react-redux'
+import * as actions from '../../store/actions/index'
 
-const latLongForm = props =>{
+
+const LatLongForm = props =>{
+
+    const [lat, setLatitude] = useState(null)
+    const [long, setLongitude] = useState(null)
+
+    const handleLatitudeChange = (event) =>{
+        setLatitude(event.target.value)
+    }
+
+    const handleLongitudeChange = (event) =>{
+        setLongitude(event.target.value)
+    }
+
+    const onSubmitHandler = (event) =>{
+        event.preventDefault()
+        props.fetchFlickrData(lat, long)
+
+    }
 
     return (
 
         <Form>
             <Form.Row>
                 <Col sm={2}>
-                    <Form.Control size="sm" type="number" step='0.0001' name="latitude" placeholder="Latitude" required={true}/>
+                    <Form.Control size="sm" type="number" step='0.0001' name="latitude" placeholder="Latitude" required={true} onChange={handleLatitudeChange}/>
                 </Col>
                 <Col sm={2}>
-                    <Form.Control size="sm" type="number" step='0.0001' name="latitude" placeholder="Latitude" required={true}/>
+                    <Form.Control size="sm" type="number" step='0.0001' name="longitude" placeholder="Longitude" required={true} onChange={handleLongitudeChange}/>
                 </Col>
-                <Button variant="outline-info" size="sm">Submit</Button>{' '}
+                <Button variant="outline-info" size="sm" onClick={onSubmitHandler}>Submit</Button>{' '}
             </Form.Row>
         </Form>
     );
 };
 
-export default latLongForm
+const mapDispatchToProps = dispatch =>{
+    return{
+        fetchFlickrData: (lat, long) => dispatch(actions.fetchFlickrData(lat, long)),
+    }
+}
+
+export default connect(null, mapDispatchToProps)(LatLongForm)
