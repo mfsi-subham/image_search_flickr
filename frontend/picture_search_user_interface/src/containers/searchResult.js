3e29054b8378960d3e5b33df93779047237f4bf9
subham mishra <subham.mishra14@gmail.com>
2020-05-26 16:18:35 +0530

    Extracting lat long of a place from db and display image data for the same

--- frontend/picture_search_user_interface/src/containers/searchResult.js
+++ frontend/picture_search_user_interface/src/containers/searchResult.js
@@ -1,36 +1,57 @@
 import React from 'react'
 import {connect} from 'react-redux'
-import Button from 'react-bootstrap/Button'
+import {Button, Form} from 'react-bootstrap'
 import * as actions from '../store/actions/index'
+import {useMutation } from '@apollo/react-hooks'
+import gql from 'graphql-tag'
 
+const ADD_FAV_URL = gql`mutation($photoUrl: String!){createFavouritePhoto(input: {photoUrl: $photoUrl}){favouritePhoto{photoUrl: photoUrl}}}`
+const DELETE_FAV_URL = gql`mutation($photoUrl: String!){deleteFavouritePhoto(input: {photoUrl: $photoUrl}){favouritePhoto{photoUrl}}}`
 
-const searchResult = (props) => {
-
+const SearchResult = (props) => {
     
-    let photoList = props.photoList.map((i, photo)=>{
-
-        let photoUrl =`https://farm${i.farm}.staticflickr.com/${i.server}/${i.id}_${i.secret}.jpg`
+    const [addFavouritePhoto] = useMutation(ADD_FAV_URL);
+    const [deleteFavouritePhoto] = useMutation(DELETE_FAV_URL);
     
+
+    const favouriteHandler = (photoUrl) => {
+        const checkboxValue = document.getElementById(photoUrl).checked
+        
+        if (checkboxValue === true){
+            addFavouritePhoto({variables: {photoUrl}}).catch(err=>{console.log(err)});
+            }
+        
+        else{
+            deleteFavouritePhoto({variables: {photoUrl}}).catch(err=>{console.log(err)});
+        };
+    };
+    
+    let photoList = props.photoList.map((i)=>{
+        let photoUrl = `https://farm${i.farm}.staticflickr.com/${i.server}/${i.id}_${i.secret}.jpg`
         return(
-            
-            <a key={i.id}target="_blank"  href={photoUrl}>
-                <img key={photoUrl} src={photoUrl} alt="not found"></img>{" "}     
-            </a>
-            
+            <div key={i.id+1}>
+                <a key={i.id+2}target="_blank"  href={photoUrl}>
+                    <img key={i.id+3} src={photoUrl} alt="not found" style={{ height: 150, width: 150 }}></img>{' '}
+                </a>
+                <Form.Check type='checkbox' key={photoUrl} id={photoUrl}  label='Add to Fav' onClick={()=>favouriteHandler(photoUrl)} />
+            </div>
 
-        ) 
-    })
+        );
+    });
 
     const nextPageHandler = (event) => {
+        //Displaying next page data
         event.preventDefault()
         props.fetchFlickrData(props.lat, props.long, props.page+1)
 
-    }
+    };
+
     const prevPageHandler = (event) => {
+        //displaying next page data
         event.preventDefault()
         props.fetchFlickrData(props.lat, props.long, props.page-1)
 
-    }
+    };
 
 
     return(
@@ -38,8 +59,8 @@ const searchResult = (props) => {
             {photoList}
             <br></br>
             <br></br>
-            {props.page !== 1? <Button variant="outline-info" size="sm" onClick={prevPageHandler}>Prev</Button>:null}{" "}
-            <Button variant="outline-info" size="sm" onClick={nextPageHandler} >Next</Button>{" "}
+            {props.page !== 1? <Button key="prev" variant="outline-info" size="sm" onClick={prevPageHandler}>Prev</Button>:null}{" "}
+            <Button key="next" variant="outline-info" size="sm" onClick={nextPageHandler} >Next</Button>{" "}            
         </div>
 
     );
@@ -59,6 +80,6 @@ const mapDispatchToProps = dispatch =>{
     return{
         fetchFlickrData: (lat, long, page) => dispatch(actions.fetchFlickrData(lat, long, page)),
     };
-}
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(searchResult)
+export default connect(mapStateToProps, mapDispatchToProps)(SearchResult)
