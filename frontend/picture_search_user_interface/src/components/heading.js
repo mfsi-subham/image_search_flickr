5db55c86d4896a068ed144f2bc93bbcad3300e5a
subham mishra <subham.mishra14@gmail.com>
2020-06-01 14:27:46 +0530

    adding component for headings

---
+++ frontend/picture_search_user_interface/src/components/heading.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const heading = () => {
+    //displaying static headings of the page
+    return (
+      <div>
+
+        <div className="p-5  bg-info text-white">
+        </div>
+
+        <div className="p-4  bg-info text-white">
+            <h1 className="display-5 text-center" >Hello, Welcome to Image Search Portal</h1>
+            <h6 className="text-center">Please select any one of the below options to display your images</h6>
+        </div>
+
+    </div>
+      
+    );
+};
+
+export default heading
